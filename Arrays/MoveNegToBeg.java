f6dea2bb520897508b490ccfee8eefc9df16719f
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-05-19 12:18:08 +0530

    Move all the negative to the beginning and positive to end with constant extra space

---
+++ Arrays/MoveNegToBeg.java
@@ -0,0 +1,36 @@
+// package Arrays;
+
+import java.util.Scanner;
+
+public class MoveNegToBeg {
+    public static int[] MoveNeg(int arr[]){
+
+        int j=0; 
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] < 0){
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+                j++; 
+            }
+        }
+        return arr; 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        int n = sc.nextInt(); 
+        int arr[] = new int[n]; 
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt(); 
+        }
+
+        int result[] = MoveNeg(arr); 
+
+        for(int i=0; i<result.length; i++){
+            System.out.print(result[i] + " ");
+        }
+
+    }
+}
