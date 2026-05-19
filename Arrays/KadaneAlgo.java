290c9f6037a06df013398e5677631bc935f202a0
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-05-19 17:15:47 +0530

    Kadane's Algorithm : MaxiMum SubArray Sum

---
+++ Arrays/KadaneAlgo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class KadaneAlgo {
+    public static int maxSubarraySum(int[] arr){
+        int MaxiMum = Integer.MIN_VALUE; 
+        int sum = 0; 
+
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i]; 
+
+            if(sum > MaxiMum){
+                MaxiMum = sum; 
+            }
+
+            if(sum < 0){
+                sum = 0; 
+            }
+        }
+        return MaxiMum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        int n = sc.nextInt(); 
+        int arr[] = new int[n]; 
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt(); 
+        }
+
+        System.out.println(maxSubarraySum(arr));
+    }
+}
