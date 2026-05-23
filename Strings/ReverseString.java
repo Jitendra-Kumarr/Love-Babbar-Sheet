b4dd1c618c81236fa36ea5efba07c77253346e8c
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-05-24 00:02:23 +0530

    344. Reverse String

---
+++ Strings/ReverseString.java
@@ -0,0 +1,31 @@
+// package Strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void reverseString(char[] s){
+        int start = 0; 
+        int end = s.length-1; 
+
+        while (start < end) {
+            char temp = s[start]; 
+            s[start] = s[end]; 
+            s[end] = temp; 
+            start++; end--; 
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        reverseString(s);
+
+        System.out.println(Arrays.toString(s));
+
+        
+        sc.close();
+    }
+}
