b2b40f867f9533f0b60cf0faae65434d613591a8
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-05-24 00:30:27 +0530

    Palindrome String

---
+++ Strings/isPalindrome.java
@@ -0,0 +1,27 @@
+// package Strings;
+
+import java.util.Scanner;
+
+public class isPalindrome {
+    static boolean isPalindrome(String str){
+        int start = 0; 
+        int end = str.length()-1; 
+
+        while(start < end){
+            if(str.charAt(start) != str.charAt(end)){
+                return false; 
+            }
+            start++; end--; 
+        }
+        return true; 
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        String str = sc.next(); 
+
+        System.out.println(isPalindrome(str));
+    }
+    
+}
