7c26876fa3becb2d933971aa06483d3bbf9032a6
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-07-03 09:12:02 +0530

    Tree Implementation

---
+++ BinaryTrees/TreeImplementation.java
@@ -0,0 +1,34 @@
+// package BinaryTrees;
+
+class Node {
+    int data;
+    Node left; 
+    Node right; 
+
+    Node(int data){
+        this.data = data; 
+    }
+}
+
+public class TreeImplementation {
+    
+    void preorder(Node root){
+        if(root == null){
+            return; 
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1); 
+        root.left = new Node(2); 
+        root.right = new Node(3);
+
+        TreeImplementation obj = new TreeImplementation(); 
+        obj.preorder(root);
+
+    }
+    
+}
